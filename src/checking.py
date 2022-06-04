5e7fe4eca32bc419a2701bb2e7e941a11a9e7e5d
andy <andy840119@gmail.com>
2022-06-04 11:50:09 +0800

    Implement checking program.

--- src/checking.py
+++ src/checking.py
@@ -1,5 +1,92 @@
-def main() -> int:
-    return 1
+import enum
+import json
+import logging
+import os
+import re
+import subprocess
 
-if __name__ == '__main__':
-    exit(main())
+logger = logging.getLogger('checking')
+
+
+class DiffFile:
+    file_name = ''
+    content = ''
+
+
+def _list_files_staged_only(extension):
+    # list staged files
+    output = subprocess.check_output(
+        ['git', '--no-pager', 'diff', '--cached', '-z', f'"{extension}"'],
+        encoding='utf-8',
+    )
+
+    # todo: get content in the file.
+    for file in tracked_files:
+        logger.debug('file aaa: %r', json.dumps(list(file)))
+
+    return set(
+        [
+            os.path.normpath(tracked_file)
+            for tracked_file in tracked_files
+            if re.compile(pattern).match(os.path.basename(tracked_file))
+        ]
+    )
+
+
+class Format(enum.Enum):
+    HTML = 'html'
+    ANGULAR = 'angular'
+    PYTHON = 'python'
+
+    def __str__(self):
+        return self.value
+
+
+class CheckMode(enum.Enum):
+    CHECK_MISSING_TRANSLATE = 'check_missing_translate'
+    GET_ALL_TRANSLATE_FILE_PATH = 'get_all_translate_file_paths'
+
+    def __str__(self):
+        return self.value
+
+
+class Checking:
+    def __init__(self, format, mode):
+        regex = self._find_tracked_files_extension(Format(format))
+
+        self._tracked_files = _list_files_staged_only(regex)
+        logger.debug('Tracked files: %r', self._tracked_files)
+
+        logger.debug('AAA: %r', json.dumps(self._tracked_files))
+
+        # todo: get content in the file.
+        for file in self._tracked_files:
+            logger.debug('file: %r', json.dumps(list(file)))
+
+        # append .git to exclude folder
+        exclude = ['.git']
+
+        # normalize a pathname by collapsing redundant separators
+        self._exclude = [os.path.normpath(path) for path in exclude]
+        self._mode = CheckMode(mode)
+
+    @staticmethod
+    def _find_tracked_files_extension(format):
+        if format == Format.HTML:
+            return '*.html'
+        elif format == Format.ANGULAR:
+            return '*.component.html'
+        elif format == Format.PYTHON:
+            return '*.py'
+        else:
+            raise Exception()
+
+    def run(self) -> int:
+        fail = 0
+
+        # todo: waiting for implementing.
+        # if self._find_for_unittest():
+        #     fail = 1
+        logger.debug('Tracked files: %r', self._tracked_files)
+
+        return fail
---
+++ tests/checking_test.py
@@ -0,0 +1,26 @@
+""" sample:
+diff --git a/index.html b/index.html
+index db38a122b8..3197d8659f 100644
+--- a/index.html
++++ b/index.html
+@@ -57,4 +57,4 @@
+
+ {% endblock %}
+
+-<div> 123 </div>
++<div> aaa </div>
+diff --git a/container-date-filter.component.html b/container-date-filter.component.html
+index 5cb09e4ea9..e1e175b654 100644
+--- a/container-date-filter.component.html
++++ b/container-date-filter.component.html
+@@ -73,4 +73,4 @@
+     </div>
+ </div>
+
+-<div> 123 </div>
++<div> aaa </div>
+"""
+
+
+def test_get_diff_line():
+    pass
