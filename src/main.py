5d8937b48e319b9bc494e0203a440b8230fe94e7
andy <andy840119@gmail.com>
2022-06-04 11:40:58 +0800

    Create new file as entry point. Should be better to separate the commend interface and executable class.

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import argparse
+import logging
+
+from src.checking import Checking, CheckMode, Format
+
+logger = logging.getLogger('checking')
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(description='Check the translate files.')
+    parser.add_argument(
+        '-q', '--quite', action='store_true', default=False, help='disable all log'
+    )
+
+    parser.add_argument(
+        '-f',
+        '--format',
+        type=Format,
+        default=Format.ANGULAR,
+        choices=list(Format)
+    )
+
+    parser.add_argument(
+        '-m',
+        '--mode',
+        type=CheckMode,
+        default=CheckMode.CHECK_MISSING_TRANSLATE,
+        choices=list(CheckMode)
+    )
+
+    args = parser.parse_args()
+
+    if args.quite:
+        logger.setLevel(logging.ERROR)
+    else:
+        logger.setLevel(logging.INFO)
+        if not logger.hasHandlers():
+            handler = logging.StreamHandler()
+            logger.addHandler(handler)
+
+    checking = Checking(args.format, args.mode or [])
+    return checking.run()
+
+
+if __name__ == '__main__':
+    exit(main())
